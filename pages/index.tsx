d1494fc8ba709018c4426582c5170a59f5693c04
정규재 <wjdrbwo1206@naver.com>
2022-11-05 20:22:57 +0900

    feat: eslint 설정 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,3 @@
-import { css } from '@emotion/react'
-import {
-  Animated,
-  Basic,
-  bounce,
-  Combined,
-  Pink,
-  BasicExtended,
-  ComponentSelectorsExtended,
-} from '../shared/styles'
+const Home = () => <div></div>;
 
-const Home = () => (
-  <div
-    css={css`
-      display: flex;
-      flex-direction: column;
-    `}
-  >
-    <Basic>Cool Styles</Basic>
-    <Pink>Pink text</Pink>
-    <Combined>
-      With <code>:hover</code>.
-    </Combined>
-    <Animated animation={bounce}>Let's bounce.</Animated>
-    <ComponentSelectorsExtended>
-      <BasicExtended>Nested</BasicExtended>
-    </ComponentSelectorsExtended>
-  </div>
-)
-
-export default Home
+export default Home;
